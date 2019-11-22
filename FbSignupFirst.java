85d09ba2d3ea801c2faab229cd5f6585de786690
aakashraj22 <47181888+aakashraj22@users.noreply.github.com>
2019-11-22 10:23:13 +0530

    Add files via upload

---
+++ FbSignupFirst.java
@@ -0,0 +1,23 @@
+package fblogin;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FbSignupFirst {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\sruthi-selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("www.google.com");
+		
+	
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
